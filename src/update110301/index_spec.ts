52b8f3056410a8aad98d722fd92cb835fb8460e0
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2021-10-04 15:36:02 +0200

    Neuen Updater 110301 angelegt.

---
+++ src/update110301/index_spec.ts
@@ -0,0 +1,69 @@
+import { callRule, SchematicContext } from '@angular-devkit/schematics';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+import { of as observableOf } from 'rxjs';
+import { getPackageJsonDependency } from '../utility/dependencies';
+import { appOptions, workspaceOptions } from '../utility/test';
+import { UtilConfig } from '../utility/util';
+import { update110301 } from './index';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('update110301', () => {
+  let appTree: UnitTestTree;
+  let runner: SchematicTestRunner;
+  let context: SchematicContext;
+
+  const testOptions: any = {};
+
+  beforeEach(async () => {
+    runner = new SchematicTestRunner('schematics', collectionPath);
+
+    appTree = await runner.runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions).toPromise();
+    appTree = await runner
+      .runExternalSchematicAsync('@schematics/angular', 'application', appOptions, appTree)
+      .toPromise();
+
+    UtilConfig.defaultWaitMS = 0;
+
+    const collection = runner.engine.createCollection(collectionPath);
+    const schematic = runner.engine.createSchematic('update-11.3.1', collection);
+    context = runner.engine.createContext(schematic);
+
+    testOptions.project = appOptions.name;
+    testOptions.path = workspaceOptions.newProjectRoot + '/' + appOptions.name;
+    testOptions.verbose = true;
+  });
+
+  describe('[Rule] update110301', () => {
+    it('Sollte die Abhängigkeiten aktualisieren', (done) => {
+      appTree.overwrite(
+        '/package.json',
+        `
+            {
+              "name": "Lorem ipsum",
+              "version": "0.0.32",
+              "scripts": {
+                "build": "tsc -p tsconfig.json",
+                "test": "npm run build && jasmine src/**/*_spec.js"
+              },
+              "dependencies": {
+                "@ihk-gfi/lux-components": "11.0.0",
+              },
+              "devDependencies": {
+                "@angular-devkit/build-angular": "0.1102.10",
+              }
+            }
+        `
+      );
+
+      callRule(update110301(testOptions), observableOf(appTree), context).subscribe(
+        () => {
+          expect(getPackageJsonDependency(appTree, '@ihk-gfi/lux-components').version).toEqual('11.3.1');
+          done();
+        },
+        (reason) => expect(reason).toBeUndefined()
+      );
+    });
+  });
+});
