1af6fccdd01a8b781f5a4e09e37b772916c20234
Thomas Dickhut <thomas.dickhut@gfi.ihk.de>
2021-11-09 12:53:11 +0100

    LUX-Components 11.4.0 releasen

---
+++ src/update110500/index.ts
@@ -0,0 +1,28 @@
+import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import * as chalk from 'chalk';
+import { updateDependency } from '../utility/dependencies';
+import { finish, messageInfoRule, messageSuccessRule } from '../utility/util';
+
+export function update110500(options: any): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    return chain([
+      messageInfoRule(`Die LUX-Components werden auf die Version 11.5.0 aktualisiert...`),
+
+      messageInfoRule(`Die Datei "package.json" wird angepasst...`),
+      updateDependencies(),
+      messageSuccessRule(`Die Datei "package.json" wurde angepasst.`),
+
+      messageSuccessRule(`Die LUX-Components wurden auf die Version 11.5.0 aktualisiert.`),
+      finish(`${chalk.yellowBright('Fertig!')}`)
+    ]);
+  };
+
+  function updateDependencies() {
+    return (tree: Tree, _context: SchematicContext) => {
+      updateDependency(tree, '@ihk-gfi/lux-components', '11.5.0');
+      updateDependency(tree, '@ihk-gfi/lux-components-theme', '11.7.1');
+
+      return tree;
+    };
+  }
+}
